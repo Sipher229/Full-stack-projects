855af2dfd698fb02794279784a3e8440e1018c19
Philippe Neri Singizwa <neriwest20@gmail.com>
2024-01-02 02:02:48 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express"
+import bodyParser from "body-parser"
+import axios from "axios"
+
+const app = express();
+const port = 3000;
+const API_URL = "https://byabbe.se/on-this-day/";
+let message = "";
+const year = new Date().getFullYear();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+function getMonth(numerical_str){
+    const months = {1:"January", 2:"February", 3:"March", 4:"April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10:"October", 11:"November", 12:"December" }
+    return months[numerical_str];
+}
+
+app.get("/", async (req, res)=>{
+    const currentDate = new Date();
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() +1;
+    const response = await axios.get(API_URL + `${month}/${day}/events.json`);
+    const {wikipedia, date, events} = response.data;
+    res.render("index.ejs", {link: wikipedia, date: "Today", events: events, message:message, year: year});
+});
+app.post("/submit", async (req, res)=>{
+    const {month, day} = req.body
+    
+    try{const response = await axios.get(API_URL + `${month}/${day}/events.json`);
+        // console.log(response.data);
+        const {wikipedia, date, events} = response.data;
+        res.render("index.ejs", {link: wikipedia, date: `${getMonth(parseInt(month))} ${day}` , events: events, year:year});}
+    catch {
+        message = "Invalid input. Verify your input and try again."
+        res.redirect("/");
+        }
+});
+
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`);
+});
